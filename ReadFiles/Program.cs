89fc60d7447cd6e6e8e6661c6224fffe36cf52ff
Charitha <Charitha.Neelisetty@ivycomptech.com>
2022-10-25 20:57:44 +0530

    Files Assignment october 19

---
+++ ReadFiles/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ReadFiles
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string file1 = @"C:\Users\Charitha.Neelisetty\OneDrive - Entain Group\Desktop\file1.txt";
+            Console.WriteLine(File.ReadAllText(file1));
+            string file2 = @"C:\Users\Charitha.Neelisetty\OneDrive - Entain Group\Desktop\file2.txt";
+            await Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine(File.ReadAllText(file2));
+            });
+        }
+    }
+}
